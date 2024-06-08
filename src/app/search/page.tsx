1b89966936b96ebfc6f580145eaf245697f5a262
ayobami paul <aybamz181@gmail.com>
2024-06-08 23:58:46 +0100

    fixing search hero  issues

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,11 +1,12 @@
 import { Header } from "@/layouts";
+import SearchHero from "@/layouts/Search/SearchHero";
 import SearchWidget from "@/layouts/Search/SearchWidget";
-import Landing from "@/layouts/landing/Landing";
 
 export default function Search() {
   return (
     <main>
       <Header />
+      <SearchHero />
       <SearchWidget />
       <p className="text-3xl font-bold min-h-screen p-4">
         Barr. Caleb Mutfwang
