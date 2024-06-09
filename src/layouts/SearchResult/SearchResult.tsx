5a0669565051a5d12388912e12ae834fc1c867b5
ayobami paul <aybamz181@gmail.com>
2024-06-09 03:35:29 +0100

    fixing search results  issues

--- src/layouts/SearchResult/SearchResult.tsx
+++ src/layouts/SearchResult/SearchResult.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { GoHome } from "react-icons/go";
 import ArticleImage from "@/assets/imgs/article1png.png";
 import Image from "next/image";
+import { AiOutlineDislike, AiOutlineLike } from "react-icons/ai";
+import { IoShareOutline } from "react-icons/io5";
 
 function SearchResult() {
   const data = {
@@ -9,6 +11,7 @@ function SearchResult() {
     title: "How to Apply for Driver’s License in Plateau State",
     date: "12 Jul, 2023",
     min: "5 Min Read",
+    tags: ["Driving", "Transportation", "FRSC", "VIO"],
     content: [
       { type: "image", image: ArticleImage, title: "" },
       {
@@ -114,6 +117,9 @@ function SearchResult() {
             <GoHome size={18} />/<p>Search</p>/<p>SEARCH RESULTS</p>/
             <p>ARTICLES</p>
           </span>
+          <button className="h-10 px-8 rounded-full bg-gray-200 border w-fit border-gray-400">
+            Guideline
+          </button>
           <p className="text-[52px] text-brand-main font-medium max-w-[800px] leading-[56px]">
             {data?.title}
           </p>
@@ -155,6 +161,66 @@ function SearchResult() {
               )}
             </span>
           ))}
+          {data?.subContent?.map((item: any, index: any) => (
+            <span key={index}>
+              <span className="grid lg:grid-cols-5 grid-cols-1 gap-5 items-start">
+                <span className="text-[#00000080] opacity-80 font-light flex items-center gap-4 text-[16px] uppercase col-span-1">
+                  <p>{item?.title}</p>
+                </span>
+                <span className="lg:col-span-4 col-span-1 flex flex-col gap-6">
+                  <p className="text-[24px] leading-[36px] font-normal text-brand-main m-0 lg:col-span-4 col-span-1">
+                    {item?.heading}
+                  </p>
+                  <p className="text-[18px] font-normal text-[#00000099] m-0 ">
+                    {item?.text}
+                  </p>
+                  <ul>
+                    {item?.lists.map((element: any, index: any) => (
+                      <li
+                        className="list-disc ml-8 pl-3 my-2 text-[#00000099]"
+                        key={index}
+                      >
+                        {element}
+                      </li>
+                    ))}
+                  </ul>
+                  {item?.text2 && (
+                    <p className="text-[18px] font-normal text-[#00000099] m-0 ">
+                      {item?.text2}
+                    </p>
+                  )}
+                  {item?.text3 && (
+                    <p className="text-[18px] font-normal text-[#00000099] m-0 ">
+                      {item?.text3}
+                    </p>
+                  )}
+                </span>
+              </span>
+            </span>
+          ))}
+        </span>
+        <span className="grid lg:grid-cols-5 grid-cols-1 gap-5">
+          <span className="col-span-1"></span>
+          <span className="flex gap-3 flex-wrap lg:col-span-4 col-span-1 ">
+            {data?.tags?.map((item: any, index: any) => (
+              <button className="h-10 px-8 rounded-full bg-gray-200 border w-fit border-gray-400">
+                {item}
+              </button>
+            ))}
+          </span>
+          <span className="col-span-1"></span>
+          <span className="text-gray-400 gap-6  lg:col-span-4 col-span-1 flex items-center mt-5">
+            <p className="flex items-center text-[14px]">
+              <AiOutlineLike size={24} />
+              42
+            </p>
+            <p className="flex items-center text-[14px]">
+              <AiOutlineDislike size={24} />
+            </p>
+            <p className="flex items-center text-[14px]">
+              <IoShareOutline size={24} />
+            </p>
+          </span>
         </span>
       </span>
     </div>
