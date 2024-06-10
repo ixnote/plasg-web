a5859807eb0ce4b57fea78793180237e3fc3cd1a
akutsang <akuutsang@gmail.com>
2024-06-10 12:34:21 +0100

    Your commit message

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/layouts";
+import Landing from "@/layouts/landing/Landing";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <Landing />
+      <p className="text-3xl font-bold min-h-screen p-4">Barr. Caleb Mutfwang</p>
+
+    </main>
+  );
+}
