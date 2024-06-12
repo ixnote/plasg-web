c7f4efb641be09c9445465f36a73a7545fd96838
ayobami paul <aybamz181@gmail.com>
2024-06-12 18:17:26 +0100

    responsiveness issues

--- src/utils/Provider.tsx
+++ src/utils/Provider.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import GeneralProvider from "../../context/GenralContext";
 
 export const queryClient = new QueryClient();
 
 function Providers({ children }: any) {
   return (
     <div>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <GeneralProvider>{children}</GeneralProvider>
+      </QueryClientProvider>
     </div>
   );
 }
