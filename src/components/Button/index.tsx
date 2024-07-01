cbf0ea217cc7ccf032a3f4403aa0c32f097aff21
ayobami paul <aybamz181@gmail.com>
2024-07-01 23:15:34 +0100

    fixing layout issues

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -14,7 +14,7 @@ const ButtonComponent = ({
   return (
     <button
       className={cn(
-        "border-[1px] border-brand-grayish flex items-center justify-center gap-4 h-10 px-8 rounded-lg font-normal text-base font-geistsans text-brand-main margin-auto transition-fx cursor-pointer hover:bg-brand-dark hover:text-brand-white hover:px-6 hover:border-brand-lightYellow",
+        "border-[1px] border-brand-grayish flex items-center justify-center gap-4 h-10 px-8 rounded-lg font-normal text-base font-geistsans text-white margin-auto transition-fx cursor-pointer hover:bg-brand-dark group-hover:text-brand-main hover:text-brand-white hover:px-6 hover:border-brand-lightYellow",
         className
       )}
       disabled={disabled}
