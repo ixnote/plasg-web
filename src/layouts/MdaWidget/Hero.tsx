fdb164584cc90f0766a36ffe36c169b1d5c490eb
ayobami paul <aybamz181@gmail.com>
2024-07-02 14:59:45 +0100

    fixing styling issues

--- src/layouts/MdaWidget/Hero.tsx
+++ src/layouts/MdaWidget/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <span className="col-span-1"></span>
           <div className="col-span-1 flex flex-col gap-4 justify-between">
             <div className="flex flex-col gap-4">
-              <div className="h-fit glass-fx flex flex-col gap-10 text-left p-8">
+              <div className="h-fit glass-fx1 flex flex-col gap-10 text-left p-8">
                 <div className="text-left flex flex-col gap-2">
                   <span className="font-medium text-5xl font-geistsans text-white outline-2">
                     Visit the{" "}
@@ -27,6 +27,9 @@ const Hero = () => {
                   today and experience the unique charm and hospitality that
                   make ours a must-visit destination.
                 </span>
+                <p className="text-white text-[20px]">
+                  Background: Riyom Rocks
+                </p>
               </div>
             </div>
           </div>
