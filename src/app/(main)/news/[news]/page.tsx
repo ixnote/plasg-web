f48899ab6337f5d7d0117329fb093c504e7ae30b
Edikan <victrudoh@gmail.com>
2024-07-21 00:47:09 +0100

    UPDATE: adjusting project structure

--- src/app/(main)/news/[news]/page.tsx
+++ src/app/(main)/news/[news]/page.tsx
@@ -1,13 +1,9 @@
-import { Header } from "@/layouts";
-import Footer from "@/layouts/Footer";
 import NewsResult from "@/layouts/NewsResult/SearchResult";
 
 export default function NewsDetails() {
   return (
     <main>
-      <Header />
       <NewsResult />
-      <Footer />
     </main>
   );
 }
--- src/app/(main)/news/page.tsx
+++ src/app/(main)/news/page.tsx
@@ -1,15 +1,10 @@
-import Footer from "@/layouts/Footer";
 import News from "@/layouts/News/News";
 import React from "react";
 
 function NewsPage() {
   return (
     <main className="bg-[#F4F4F4]">
-      {/* <Header /> */}
       <News />
-      <div className="p-2 rounded-xl overflow-hidden flex">
-        <span>{/* <Footer /> */}</span>
-      </div>
     </main>
   );
 }
