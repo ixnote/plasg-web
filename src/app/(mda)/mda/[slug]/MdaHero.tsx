68c8da186986e9e57d925bc17fe891456ead3ad8
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-09-02 18:03:43 +0100

    update

--- src/app/(mda)/mda/[slug]/MdaHero.tsx
+++ src/app/(mda)/mda/[slug]/MdaHero.tsx
@@ -1,65 +1,66 @@
-import Image from "next/image";
-import React from "react";
-import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
 const MdaHero = ({ slug, hero }: any) => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col py-24 pt-[170px] gap-4 lg:gap-16">
-      <span className="flex flex-col gap-8 mb-10">
-        <span className="flex flex-col-reverse items-start justify-between gap-8 lg:flex-row">
-          <div className="flex flex-col items-start justify-start gap-8">
-            <p className="text-[52px] text-brand-main font-medium font-geistsans max-w-[800px] leading-[56px] lg:col-span-3 col-span-2">
+    <div className='flex flex-col py-24 pt-[170px] gap-4 lg:gap-16'>
+      <span className='flex flex-col gap-8 mb-10'>
+        <span className='flex flex-col-reverse items-start justify-between gap-8 lg:flex-row'>
+          <div className='flex flex-col items-start justify-start gap-8'>
+            <p className='text-[52px] text-brand-main font-medium font-geistsans max-w-[800px] leading-[56px] lg:col-span-3 col-span-2'>
               {hero?.title}
             </p>
-            <div className="w-full font-geistsans font-normal text-lg text-brand-dark/70 lg:w-[50%]">
+            <div className='w-full font-geistsans font-normal text-lg text-brand-dark/70 lg:w-[50%]'>
               {hero?.description}
             </div>
-            <div className="flex items-center justify-start">
+            <div className='flex items-center justify-start'>
               <div
                 className={
-                  "group transition-fx border-[1px] border-brand-grayish flex items-center justify-center gap-4 p-4 px-8 rounded-lg font-normal text-base font-geistsans text-brand-white cursor-pointer bg-brand-main hover:bg-brand-lightYellow hover:text-brand-dark hover:px-6 hover:border-brand-lightYellow"
+                  'group transition-fx border-[1px] border-brand-grayish flex items-center justify-center gap-4 p-4 px-8 rounded-lg font-normal text-base font-geistsans text-brand-white cursor-pointer bg-brand-main hover:bg-brand-lightYellow hover:text-brand-dark hover:px-6 hover:border-brand-lightYellow'
                 }
                 onClick={() => router.push(`/mda/${slug}/library`)}
               >
                 View Library
                 <svg
-                  width="18"
-                  height="18"
-                  viewBox="0 0 18 18"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+                  width='18'
+                  height='18'
+                  viewBox='0 0 18 18'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
                 >
                   <path
-                    className="svg-path group-hover:fill-brand-dark/80"
-                    d="M0 10.1364L0 7.86364H13.6364L7.38636 1.61364L9 0L18 9L9 18L7.38636 16.3864L13.6364 10.1364H0Z"
-                    fill="#D1D5DB"
+                    className='svg-path group-hover:fill-brand-dark/80'
+                    d='M0 10.1364L0 7.86364H13.6364L7.38636 1.61364L9 0L18 9L9 18L7.38636 16.3864L13.6364 10.1364H0Z'
+                    fill='#D1D5DB'
                   />
                 </svg>
               </div>
             </div>
           </div>
-          <span className="col-span-2 hidden flex-col gap-6 lg:flex">
-            <p className="text-[18px] font-normal text-brand-main m-0 min-w-[200px] max-w-[800px] font-geistsans">
+          <span className='col-span-2 hidden flex-col gap-6 lg:flex'>
+            <p className='relative text-[18px] font-normal text-brand-main m-0 min-w-[200px] max-w-[800px] font-geistsans'>
               <Image
                 src={hero?.logo}
                 width={220}
                 height={209}
-                alt="LOGO"
-                className="w-max h-[209px]"
+                alt='LOGO'
+
+                // className="w-max h-[209px]"
               />
             </p>
           </span>
         </span>
       </span>
-      <div className="w-full">
+      <div className='w-full'>
         <Image
           src={hero?.image}
           width={1360}
           height={420}
-          alt="mda_hero_image"
-          className="w-[1360px] h-[580px] object-cover rounded-xl xl:w-full"
+          alt='mda_hero_image'
+          className='w-[1360px] h-[580px] object-cover rounded-xl xl:w-full'
           // loading="lazy"
         />
       </div>
--- src/app/(mda)/mda/[slug]/about/page.tsx
+++ src/app/(mda)/mda/[slug]/about/page.tsx
@@ -1,33 +1,32 @@
-"use client";
+'use client';
 
-import React from "react";
-import Image from "next/image";
-import { Team } from "../../data";
-import SecondCard from "../../secondCard";
-import Footer from "@/layouts/Footer";
-import bg from "@/assets/imgs/bg-img.svg";
-import { useGeneralContext } from "../../../../../../context/GenralContext";
-import SectionDividerLight from "@/components/SectionDividerLight";
+import React from 'react';
+import Image from 'next/image';
 
-import dynamic from "next/dynamic";
-import "react-quill/dist/quill.bubble.css";
-const QuillEditor = dynamic(() => import("react-quill"), { ssr: false });
+import SecondCard from '../../secondCard';
+
+import { useGeneralContext } from '../../../../../../context/GenralContext';
+import SectionDividerLight from '@/components/SectionDividerLight';
+import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.bubble.css';
+const QuillEditor = dynamic(() => import('react-quill'), { ssr: false });
+
 const About = () => {
   const { oneMda }: any = useGeneralContext();
 
   return (
     <>
-      <div className="min-h-screen max-w-[2040px] pb-[96px] mt-[170px] relative flex items-center justify-center bg-cover bg-center bg-no-repeat">
-        <div className="m-auto w-[92%] flex justify-between flex-col gap-8">
+      <div className='min-h-screen max-w-[2040px] pb-[96px] mt-[170px] relative flex items-center justify-center bg-cover bg-center bg-no-repeat'>
+        <div className='m-auto w-[92%] flex justify-between flex-col gap-8'>
           <div
             // className=" mt-20 h-[144px]  w-full flex flex-col items-center"
-            className="mt-12 w-full flex flex-col gap-4 items-center justify-center bg-no-repeat bg-cover"
+            className='mt-12 w-full flex flex-col gap-4 items-center justify-center bg-no-repeat bg-cover'
           >
-            <div className="w-full h-[56px] flex justify-center  font-semibold font-geistsans size-[52px] text-3xl text-brand-main">
+            <div className='w-full h-[56px] flex justify-center  font-semibold font-geistsans size-[52px] text-3xl text-brand-main'>
               <p>About</p>
             </div>
-            <div className="w-full font-geistsans flex justify-center text-center leading-[28px] text-[#00000070]">
-              <p className="text-brand-dark font-geistsans">
+            <div className='w-full font-geistsans flex justify-center text-center leading-[28px] text-[#00000070]'>
+              <p className='text-brand-dark font-geistsans'>
                 {oneMda?.about?.title}
               </p>
             </div>
@@ -39,33 +38,33 @@ const About = () => {
                 growth.
               </p>
             </div> */}
-            <div className="w-full mt-16">
+            <div className='w-full mt-16'>
               <Image
                 src={oneMda?.about?.image}
                 width={1360}
                 height={580}
-                alt="mda_hero_image"
-                className="w-[1360px] h-[580px] object-cover rounded-xl xl:w-full"
+                alt='mda_hero_image'
+                className='w-[1360px] h-[580px] object-cover rounded-xl xl:w-full'
                 // loading="lazy"
               />
             </div>
           </div>
           {/* About */}
-          <div className="w-full mt-16 flex flex-col gap-8 justify-between py-8 mb-8 lg:flex-row">
-            <div className="w-full flex flex-col lg:gap-4 lg:px-6 lg:w-[40%]">
-              <span className="w-max mb-8 text-brand-main font-normal text-base font-geistmono px-2 py-1 border-[1px] border-brand-main rounded">
+          <div className='w-full mt-16 flex flex-col gap-8 justify-between py-8 mb-8 lg:flex-row'>
+            <div className='w-full flex flex-col lg:gap-4 lg:px-6 lg:w-[40%]'>
+              <span className='w-max mb-8 text-brand-main font-normal text-base font-geistmono px-2 py-1 border-[1px] border-brand-main rounded'>
                 About
               </span>
-              <p className="w-full flex justify-start items-center font-semibold text-brand-main font-geistsans text-2xl size-[50px]  my-2 lg:text-[30px] lg:leading-[48px] lg:tracking-[-2%]">
+              <p className='w-full flex justify-start items-center font-semibold text-brand-main font-geistsans text-2xl size-[50px]  my-2 lg:text-[30px] lg:leading-[48px] lg:tracking-[-2%]'>
                 {oneMda?.about?.title}
               </p>
             </div>
 
-            <div className="w-full font-geistsans text-lg font-normal lg:text-lg lg:w-[50%]">
+            <div className='w-full font-geistsans text-lg font-normal lg:text-lg lg:w-[50%]'>
               {
                 <QuillEditor
                   value={oneMda?.about?.description}
-                  theme="bubble"
+                  theme='bubble'
                   readOnly
                 />
               }
@@ -74,21 +73,21 @@ const About = () => {
           </div>
           <SectionDividerLight />
           {/* Vision, mission */}
-          <div className="w-full my-16">
-            <div className="flex flex-col gap-16 w-full lg:px-10 lg:gap-4 lg:flex-row">
-              <div className="w-full lg:w-[50%]">
-                <div className="h-full w-full ">
-                  <header className="w-[66px] h-[30px] flex justify-center items-center border-brand-main border-2 text-brand-main font-normal leading-[28px] size-[18px] font-[Geist] rounded-[4px] mb-2">
+          <div className='w-full my-16'>
+            <div className='flex flex-col gap-16 w-full lg:px-10 lg:gap-4 lg:flex-row'>
+              <div className='w-full lg:w-[50%]'>
+                <div className='h-full w-full '>
+                  <header className='w-[66px] h-[30px] flex justify-center items-center border-brand-main border-2 text-brand-main font-normal leading-[28px] size-[18px] font-[Geist] rounded-[4px] mb-2'>
                     Vision
                   </header>
-                  <p className="w-full h-[100px] flex justify-start items-center text-brand-main font-[Geist] text-4xl size-10 font-medium leading-[48px]">
+                  <p className='w-full h-[100px] flex justify-start items-center text-brand-main font-[Geist] text-4xl size-10 font-medium leading-[48px]'>
                     Our Vision
                   </p>
-                  <p className="w-full font-geistsans text-lg font-normal text-brand-dark ">
+                  <p className='w-full font-geistsans text-lg font-normal text-brand-dark '>
                     {
                       <QuillEditor
                         value={oneMda?.about?.vision}
-                        theme="bubble"
+                        theme='bubble'
                         readOnly
                       />
                     }
@@ -96,19 +95,19 @@ const About = () => {
                   </p>
                 </div>
               </div>
-              <div className="w-full lg:w-[50%]">
-                <div className="h-full w-full ">
-                  <header className="w-[66px] h-[30px] flex justify-center items-center border-brand-main border-2 text-brand-main  font-normal leading-[28px] size-[18px] font-[Geist] rounded-[4px] mb-2">
+              <div className='w-full lg:w-[50%]'>
+                <div className='h-full w-full '>
+                  <header className='w-[66px] h-[30px] flex justify-center items-center border-brand-main border-2 text-brand-main  font-normal leading-[28px] size-[18px] font-[Geist] rounded-[4px] mb-2'>
                     Mission
                   </header>
-                  <p className="w-full h-[100px] flex justify-start items-center text-brand-main font-[Geist] text-4xl size-10 font-medium leading-[48px]">
+                  <p className='w-full h-[100px] flex justify-start items-center text-brand-main font-[Geist] text-4xl size-10 font-medium leading-[48px]'>
                     Our Mission
                   </p>
-                  <p className="w-full font-geistsans text-lg font-normal text-brand-dark ">
+                  <p className='w-full font-geistsans text-lg font-normal text-brand-dark '>
                     {
                       <QuillEditor
                         value={oneMda?.about?.mission}
-                        theme="bubble"
+                        theme='bubble'
                         readOnly
                       />
                     }
@@ -120,26 +119,26 @@ const About = () => {
           </div>
           <SectionDividerLight />
           {/* Team */}
-          <div className="w-full my-8">
-            <div className="w-full h-full">
-              <div className="mt-20 mb-8 w-full flex flex-col items-center">
-                <header className="w-[100px] h-[40px] flex justify-center items-center border-brand-main border-2 text-brand-main font-normal leading-3 size-4 font-geistsans rounded-md mb-4">
+          <div className='w-full my-8'>
+            <div className='w-full h-full'>
+              <div className='mt-20 mb-8 w-full flex flex-col items-center'>
+                <header className='w-[100px] h-[40px] flex justify-center items-center border-brand-main border-2 text-brand-main font-normal leading-3 size-4 font-geistsans rounded-md mb-4'>
                   Vision
                 </header>
 
-                <div className="w-full h-[76px] flex justify-center p-4  font-semibold font-geistsans size-[52px] text-3xl text-brand-main">
+                <div className='w-full h-[76px] flex justify-center p-4  font-semibold font-geistsans size-[52px] text-3xl text-brand-main'>
                   <p>Our Team</p>
                 </div>
-                <div className="w-full font-geistsans flex justify-center text-center leading-[28px] text-[#00000070]">
-                  <p className="text-brand-dark font-geistsans">
+                <div className='w-full font-geistsans flex justify-center text-center leading-[28px] text-[#00000070]'>
+                  <p className='text-brand-dark font-geistsans'>
                     Stay connected with the latest from {oneMda?.abbreviation}.
                     Get brief updates on new projects, collaborations, and
                     innovations shaping our state.
                   </p>
                 </div>
               </div>
-              <div className="w-full flex justify-center mx-auto lg:p-8">
-                <div className="w-full grid grid-cols-1 gap-12 lg:grid-cols-3 lg:mx-auto lg:gap-x-16">
+              <div className='w-full flex justify-center mx-auto lg:p-8'>
+                <div className='w-full grid grid-cols-1 gap-12 lg:grid-cols-3 lg:mx-auto lg:gap-x-16'>
                   {oneMda?.team.map((item: any, i: number) => (
                     <SecondCard
                       key={i}
