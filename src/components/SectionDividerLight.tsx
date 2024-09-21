1cd980ec32fce8a1b2534eacd8b271a2ba7f4f7d
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-09-21 14:34:08 +0100

    update

--- src/components/SectionDividerLight.tsx
+++ src/components/SectionDividerLight.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 const SectionDividerLight = () => {
   return (
     <>
-      <div className="flex items-center justify-center py-4  bg-brand-white">
+      <div className='flex items-center justify-center py-1  bg-brand-white'>
         {/* content */}
-        <div className="flex items-center justify-between gap-4 w-[92%]">
-          <span className="w-full bg-brand-grayish p-[0.3px] rounded"></span>
+        <div className='flex items-center justify-between gap-4 w-[92%]'>
+          <span className='w-full bg-brand-grayish p-[0.3px] rounded'></span>
         </div>
       </div>
     </>
