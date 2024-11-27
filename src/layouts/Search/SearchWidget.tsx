6f03674ed77e9178fd7f10278f4021d715b40f5e
ayobami paul <aybamz181@gmail.com>
2024-11-27 18:35:50 +0100

    fixing issues 1-5

--- src/layouts/Search/SearchWidget.tsx
+++ src/layouts/Search/SearchWidget.tsx
@@ -16,7 +16,7 @@ function SearchWidget({
   return (
     <>
       <div className="py-[100px] bg-brand-main flex gap-8 flex-col p-5">
-        {data?.length > 0 ? (
+        {data?.length > 0 && (
           <>
             <span className="grid gap-5 lg:grid-cols-2 grid-cols-1 max-w-[1500px] mx-auto w-full">
               {data?.map((item: any) => (
@@ -35,7 +35,8 @@ function SearchWidget({
               />
             </span>
           </>
-        ) : (
+        )}
+        {data?.length === 0 && (
           <span className="flex w-full justify-center items-center text-white">
             <p className="capitalize text-[32px]">
               No Available Result for {active}
